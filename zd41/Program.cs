cd6afc14bb484e566f34cc907792a53a95bdf86e
Vika <vikka-94@mail.ru>
2022-12-24 19:03:53 +0300

    dz

---
+++ zd41/Program.cs
@@ -0,0 +1,17 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите числа через пробел : ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"колличество чисел больше 0 = {count}");
